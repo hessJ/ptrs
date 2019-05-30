80b686a7eee2d7c5c6c7b55db6e3724f574a0be3
Jonathan Hess <hessjo@upstate.edu>
2019-05-30 12:16:10 -0400

    Update ptrs.R

--- ptrs.R
+++ ptrs.R
@@ -11,19 +11,23 @@ e-mail: hessjo@upstate.edu
 cat(masthead)
 
 # --- load packages
+suppressMessages(require(plyr))
 suppressMessages(require(data.table))
 
-# -- functions
-load_weights = function(file, gene_col, weight_col, p_col, sep="\t", header = T){
+# -- Load gene weights for a phentype and format columns
+load_weights = function(file, gene_col, weight_col, p_col, sep="\t", header = T, calc_weight = FALSE, beta_col = '', se_col = ''){
   # -- check parameters exist
   if(is.null(file) == TRUE){stop("Weight file required!")}
   if(is.null(gene_col) == TRUE){stop("Column denoting gene name required!")}
-  if(is.null(weight_col) == TRUE){stop("Column denoting weight required!")}
   if(is.null(p_col) == TRUE){stop("Column denoting p-value required!")}
   
   # -- load file
   load_file =  fread(file, header = header, sep=sep)
   
+  weight_file <<- load_file
+  
+  if(calc_weight == FALSE){
+    if(is.null(weight_col) == TRUE){stop("Column denoting weight required!")}
   # -- keep columns required for scoring
   munge_file <<- load_file[,colnames(load_file) %in% c(gene_col, weight_col, p_col),with=FALSE]
   
@@ -32,13 +36,34 @@ load_weights = function(file, gene_col, weight_col, p_col, sep="\t", header = T)
   names(munge_file)[names(munge_file) %in% weight_col] = "weight"
   names(munge_file)[names(munge_file) %in% p_col] = "pvalue"
   
+  } 
+  
+  if(calc_weight == TRUE){
+    
+    if(beta_col == ""){stop("Warning! Column with beta coefficient required to calculate weight.")}
+    if(se_col == ""){stop("Warning! Column with standard error required to calculate weight.")}
+    
+    load_file$weight = load_file[,colnames(load_file) %in% beta_col,with=FALSE]/load_file[,colnames(load_file) %in% se_col,with=FALSE]
+    
+    munge_file <<- load_file[,colnames(load_file) %in% c(gene_col, "weight", p_col),with=FALSE]
+    
+    # -- rename columns
+    names(munge_file)[names(munge_file) %in% gene_col] = "gene_id"
+    names(munge_file)[names(munge_file) %in% p_col] = "pvalue"
+    
+  }
+  
   # -- object available to environment 
-  ptrs_weights <<- munge_file[order(munge_file$pvalue, decreasing = FALSE), ]
+  return(munge_file[order(munge_file$pvalue, decreasing = FALSE), ])
 }
 
-ptrs = function(dat, p_thres = c(0.001, 0.01, 0.05, 0.1, 0.5, 1.0), scale_scores = TRUE){
+# Function to calculate risk scores using PTRS algorithm
+ptrs = function(dat = NULL, weight_table = NULL, p_thres = c(0.001, 0.01, 0.05, 0.1, 0.5, 1.0), scale_scores = TRUE){
   # -- check existance of parameters
   if(is.null(dat) == TRUE){stop("Data frame with gene expression data required!")}
+  if(is.null(weight_table) == FALSE) {
+    ptrs_weights = weight_table
+  } else {stop("Warning! A data frame with gene weights is required.")}
   
   # -- format as data frame
   dat = data.frame(dat)
@@ -49,7 +74,8 @@ ptrs = function(dat, p_thres = c(0.001, 0.01, 0.05, 0.1, 0.5, 1.0), scale_scores
   
   # -- reorder dat according to munge_file
   dat = dat[rownames(dat) %in% common_gene_id, ]
-  munge_file = munge_file[munge_file$gene_id %in% common_gene_id, ]
+  munge_file = ptrs_weights[ptrs_weights$gene_id %in% common_gene_id, ]
+  
   if(nrow(munge_file) != nrow(dat)){stop("Unexpected number of rows. Exiting.")}
   dat = dat[match(munge_file$gene_id, rownames(dat)), ]
   
@@ -57,7 +83,7 @@ ptrs = function(dat, p_thres = c(0.001, 0.01, 0.05, 0.1, 0.5, 1.0), scale_scores
   scores = list()
   for(x in 1:length(p_thres)){
     message("\rScoring threshold: ", p_thres[[x]])
-    weight_sub = ptrs_weights[ptrs_weights$pvalue <= p_thres[[x]], ]
+    weight_sub = munge_file[munge_file$pvalue <= p_thres[[x]], ]
     dat_sub = dat[rownames(dat) %in% weight_sub$gene_id, ]
     dat_sub = data.frame(t(dat_sub))
     scoreMatrix = sweep(dat_sub, MARGIN = 2, STATS = weight_sub$weight, `*`)
